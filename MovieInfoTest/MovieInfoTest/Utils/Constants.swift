7e6eb991ec9d82c14298dcff4cf308dffae7ed4b
Randy <o1984531@gmail.com>
2021-08-21 21:26:04 +0800

    Add the image url prefix for small and large size

--- MovieInfoTest/MovieInfoTest/Utils/Constants.swift
+++ MovieInfoTest/MovieInfoTest/Utils/Constants.swift
@@ -3,4 +3,7 @@ import Foundation
 class Constants {
     static let BASE_URL:String = "http://api.themoviedb.org"
     static let API_KEY:String = "328c283cd27bd1877d9080ccb1604c91"
+    
+    static let IMG_SMALL_URL_PREFIX:String = "https://image.tmdb.org/t/p/w200/"
+    static let IMG_LARGE_URL_PREFIX:String = "https://image.tmdb.org/t/p/w500/"
 }
