1f1dc6e09a87f15ca67d01b46fa773ddab2191b7
Randy <o1984531@gmail.com>
2021-08-21 23:07:28 +0800

    Add MovieListModel

---
+++ MovieInfoTest/MovieInfoTest/Flow/MovieList/Model/MovieListModel.swift
@@ -0,0 +1,25 @@
+import Foundation
+import RxSwift
+
+class MovieListModel {
+    
+    private var disposeBag:DisposeBag = DisposeBag()
+    var movieListItemInfos:BehaviorSubject<[MovieListItemInfo]> = BehaviorSubject(value: [])
+    
+    func fetchMovieList(date:String, sortType:String, page:Int) {
+        let req:MovieListRequest = MovieListRequest(
+            apiKey: Constants.API_KEY,
+            primaryReleaseDate: date,
+            sortBy: sortType,
+            page: page
+        )
+        
+        ApiUtils.requestApi(apiType: MovieInfoApi.MovieList(ApiUtils.getBodyParams(obj: req)), responseType: MovieSummaryInfo.self) { movieSummaryInfo in
+            self.movieListItemInfos.onNext(movieSummaryInfo.results)
+        } onFail: { error in
+            print("Error")
+        } onFinally: {
+            print("Finish")
+        }.disposed(by: disposeBag)
+    }
+}
