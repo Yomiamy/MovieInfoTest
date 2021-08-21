044e29f6d31e727fba25e238285129feb8b4f64b
Randy <o1984531@gmail.com>
2021-08-21 14:33:32 +0800

    Add API test

--- MovieInfoTest/MovieInfoTest/ViewController.swift
+++ MovieInfoTest/MovieInfoTest/ViewController.swift
@@ -11,7 +11,27 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // API Test
+//        let req:MovieListRequest = MovieListRequest(
+//            apiKey: Constants.API_KEY,
+//            primaryReleaseDate: "2016-12-31",
+//            sortBy: "release_date.desc",
+//            page: 1
+//        )
+        
+        let req:MovieDetailRequest = MovieDetailRequest(
+            apiKey: Constants.API_KEY
+        )
+        
+        let _ = ApiUtils.requestApi(apiType: MovieInfoApi.MovieDetail("328111", ApiUtils.getBodyParams(obj: req)),
+                                    responseType: MovieDetailInfo.self) { data in
+            print("Request Success")
+        } onFail: { error in
+            print("Error")
+        } onFinally: {
+            print("Finish")
+        }
+
     }
 
 
