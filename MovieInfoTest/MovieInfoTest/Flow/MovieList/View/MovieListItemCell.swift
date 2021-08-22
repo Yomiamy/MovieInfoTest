4e5c9055416cb1bc81b5464deec2e9b80e1b9c87
Randy <o1984531@gmail.com>
2021-08-22 18:03:22 +0800

    Add the release date and vote for information in the cell

--- MovieInfoTest/MovieInfoTest/Flow/MovieList/View/MovieListItemCell.swift
+++ MovieInfoTest/MovieInfoTest/Flow/MovieList/View/MovieListItemCell.swift
@@ -3,20 +3,25 @@ import Kingfisher
 
 class MovieListItemCell: UITableViewCell {
     
+    @IBOutlet weak var releaseDateLabel: UILabel!
     @IBOutlet weak var posterImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var popularityLabel: UILabel!
+    @IBOutlet weak var voteForLabel: UILabel!
     
-
     func setData(movieListItemInfo:MovieListItemInfo) {
+        let releaseDate = movieListItemInfo.releaseDate
         let imgUrl = movieListItemInfo.imageUrl
         let url = "\(Constants.IMG_SMALL_URL_PREFIX)\(imgUrl)"
         let title = movieListItemInfo.title
         let popularity = movieListItemInfo.popularity
+        let voteFor = "\(movieListItemInfo.voteCount)"
         
+        self.releaseDateLabel.text = releaseDate
         self.titleLabel.text = title
         self.popularityLabel.text = "\(popularity)"
         self.posterImageView.kf.setImage(with: URL(string: url))
+        self.voteForLabel.text = voteFor
     }
 
 }
