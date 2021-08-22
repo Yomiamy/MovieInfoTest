a678109fe453fdef2e5188a64f03fefd4a7af5e5
Randy <o1984531@gmail.com>
2021-08-23 00:11:34 +0800

    Add Movie Detail Page

---
+++ MovieInfoTest/MovieInfoTest/Flow/MovieDetail/View/MovieDetailTableViewController.swift
@@ -0,0 +1,29 @@
+import UIKit
+import RxSwift
+
+class MovieDetailTableViewController: UITableViewController {
+
+    var id:String!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.initView()
+        self.initData()
+        self.bindingData()
+        
+    }
+    
+    func initView() {
+        
+    }
+    
+    func initData() {
+    }
+    
+    func bindingData() {
+        
+    }
+
+}
