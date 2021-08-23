fb990e504e6da4aaa47cf1aab7236bbc54a7aee4
Randy <o1984531@gmail.com>
2021-08-23 17:47:50 +0800

    Add bookMovieUrl property
    Add bookMovieUrl property

--- MovieInfoTest/MovieInfoTest/Utils/Constants.swift
+++ MovieInfoTest/MovieInfoTest/Utils/Constants.swift
@@ -6,6 +6,7 @@ class Constants {
     
     static let IMG_SMALL_URL_PREFIX:String = "https://image.tmdb.org/t/p/w200/"
     static let IMG_LARGE_URL_PREFIX:String = "https://image.tmdb.org/t/p/w500/"
+    static let BOOK_MOVIE_URL_PREFIX:String = "https://www.themoviedb.org/movie/"
     
     static let RELEASE_DATE = "2016-12-31"
     static let ROUTE_SHOW_MOVIE_DETAIL = "show_movie_detail"
