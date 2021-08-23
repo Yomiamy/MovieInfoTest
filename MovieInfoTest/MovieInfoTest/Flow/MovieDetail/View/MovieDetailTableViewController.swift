dd6d426ef2d73e5d54f2a3a256516cfd4128f4c2
Randy <o1984531@gmail.com>
2021-08-23 11:54:30 +0800

    Assign init data to detail page

--- MovieInfoTest/MovieInfoTest/Flow/MovieDetail/View/MovieDetailTableViewController.swift
+++ MovieInfoTest/MovieInfoTest/Flow/MovieDetail/View/MovieDetailTableViewController.swift
@@ -1,13 +1,21 @@
 import UIKit
 import RxSwift
+import Kingfisher
 
 class MovieDetailTableViewController: UITableViewController {
 
-    var id:String!
+    @IBOutlet weak var movieNameLabel: UILabel!
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var genresLabel: UILabel!
+    @IBOutlet weak var languageLabel: UILabel!
+    @IBOutlet weak var releaseDataLabel: UILabel!
     
+
     private var disposeBag:DisposeBag = DisposeBag()
     private var viewModel:MovieDetailViewModel!
     var movieDetailInfo:PublishSubject<MovieDetailInfo> = PublishSubject()
+    var movieListItemInfo:MovieListItemInfo!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,14 +28,18 @@ class MovieDetailTableViewController: UITableViewController {
     }
     
     func initView() {
+        self.navigationItem.title = self.movieListItemInfo.title
+        self.movieNameLabel.text = self.movieListItemInfo.title
+        self.releaseDataLabel.text = self.movieListItemInfo.releaseDate
         
+        self.posterImageView.kf.setImage(with: URL(string: self.movieListItemInfo.imageUrl))
     }
     
     func initData() {
+        self.viewModel = MovieDetailViewModel(view: self)
     }
     
     func bindingData() {
-        
         self.viewModel
             .movieDetailInfo
             .subscribe { controlEvent in
